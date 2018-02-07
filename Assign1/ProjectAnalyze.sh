8caf683c86d3d6626fc2515ff1970e6816462b50
Connor David Hewick <hewicc1@mills.cas.mcmaster.ca>
2018-02-07 10:08:09 -0500

    added ProjectAnalyze.sh

---
+++ Assign1/ProjectAnalyze.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# bash assignment
+
+# Informs you if you're local repo is up to date with the remote repo (Hint: theres already a git command to do this, just find it)
+
+
+
+# Puts all uncommited changes in a file changes.log (Hint: just redirect the output of a certain git command)
+
+
+
+
+# Puts each line from every file of your project with the tag #TODO into a file todo.log
+
+
+
+
+# Checks all haskell files for syntax errors and puts the results into error.log (Hint: ghc -fno-code file.hs)
+
+
+
+
+# Add at least one feature of your own design
+
+
+
+
+# Document the script, how it's used and a description of each feature in the Assign1 README file
+
+
+
+
+# Feel free to look at other peoples Assignments on GitHub and integrate useful features they added into your own, but you must reference who you got the feature from in your README
+
+
+
+
+
+
+
