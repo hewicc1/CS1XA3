46c4660f4c8b9deb864a2aaa9d9d440bc461d4f3
ConnorHewick-mills <hewicc1@mills.cas.mcmaster.ca>
2018-02-25 21:02:02 -0500

    update

--- Assign1/ProjectAnalyze.sh
+++ Assign1/ProjectAnalyze.sh
@@ -3,40 +3,39 @@
 # bash assignment
 
 # Informs you if you're local repo is up to date with the remote repo (Hint: theres already a git command to do this, just find it)
-
+up_to_date() { #corresponds to requirement #1 in readme
 git diff --staged
-
+} 
 
 # Puts all uncommited changes in a file changes.log (Hint: just redirect the output of a certain git command)
-
+uncommited_changes() { #corresponds to requirement #2 in readme
 git status > changes.log
+git diff > changes2.log
+}
 
 
 # Puts each line from every file of your project with the tag #TODO into a file todo.log
-
-git grep "#TODO" > todo.log
-
+todo() {
+echo "Total "#TODO"'s Found:" > todo.log
+git grep --count "#TODO" >> todo.log
+echo "" >> todo.log
+echo "Instances Found:" >> todo.log
+git grep "#TODO" >> todo.log
+}
 
 # Checks all haskell files for syntax errors and puts the results into error.log (Hint: ghc -fno-code file.hs)
-
+haskell_errors() {
 #find . -name "*?.hs" | xargs -I {} ghc -fno-code "{}" > error.log
-
-
-# Add at least one feature of your own design
-
-
-
-
-# Document the script, how its used and a description of each feature in the Assign1 README file
-
-	
-
-
-# Feel free to look at other peoples Assignments on GitHub and integrate useful features they added into your own, but you must reference who you got the feature from in your README
+#ghc -fno-code *?.hs > error.log
+echo hi
+}
 
 
 
-
-
+# Run all Functions:
+up_to_date
+uncommited_changes
+todo
+haskell_errors
 
 
--- Assign1/readme.md
+++ Assign1/readme.md
@@ -2,11 +2,26 @@
 ## Goal:
 Create a bash script ProjectAnalyze.sh to kepp at the root of your git repo that at least the following functionality:
 ### Requirements:
-1. Informs you if you're local repo is up to date with the remote repo
+1. Informs you if your local repo is up to date with the remote repo
 2. Puts all uncommited changes in a file changes.log
 3. Puts each line from every file of your project with the tag #TODO into a file todo.log
 4. Checks all haskell files for syntax errors and puts the results into error.log
 ### Addition Features:
 1. File size breakdown?
+### Instructions:
+1. Keep and run script in root of git repo
+2. Will notify if your local repo is upto date with remote repo. If not up to date you must execute git pull to update
+3. Will create changes.log that has all uncommited changes
+4. Will create todo.log that has the total number "of #TODO's" in files as well as each instance that is found
+5. Will create error.log that will show syntax errors of all .hs (haskell) files
+### Functions:
+#### up_to_date
+
+#### uncommited_changes
+
+#### todo
+
+#### haskell_errors
+
 ### Citations:
 test.com
