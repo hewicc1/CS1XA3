a25d77e46c67a6e8560995cbd2dac2cbcd41741c
ConnorHewick-mills <hewicc1@mills.cas.mcmaster.ca>
2018-03-31 13:17:46 -0400

    update to elm cv

--- Assign2/ConnorCV.elm
+++ Assign2/ConnorCV.elm
@@ -2,7 +2,7 @@
 import Html exposing (..)
 import Html.Attributes exposing (..)
 
-divStyle = style [("background-image", "url(CV_Photos/b.jpg)")
+divStyle = style [("background-image", "url(b.jpg)")
                   ,("border-style","solid")
                   ,("font-family","Times New Roman")
                   ,("padding","0 20% 1em 20%")]
@@ -26,28 +26,28 @@ main = div [divStyle] [
               
               header [headerStyle] [
                                     h1 [] [text "Connor Hewick"]
-                                    ,text "connor@hewick.ca"]
+                                    ,text "hewicc1@mcmaster.ca"]
 
               ,section [mainStyle] [
                                     h3 [] [text "Objective:"]
                                     , p [] [text "I am a university student who has passion to learn new skills, techniques and how things work within the professional world. I am able to work and collaborate within a team environment, as well as work individually while striving to be time efficient and maintain work quality. I am most comformable with programming with Python and Swift."]
                                     ]
 
-              ,img [src "CV_Photos/connor123.jpg",style [("width","100%")]] []
+              ,img [src "/connor123.jpg",style [("width","100%")]] []
 
               ,aside [skillsStyle] [
                           text "Skills:"
                           ,ul [] [
                                 li [] [text "Using Python"]
-                                ,img [src "CV_Photos/4_stars.png",style [("width","100%")]] []
+                                ,img [src "4_stars.png",style [("width","100%")]] []
                                 ,li [] [text "Using Swift"]
-                                ,img [src "CV_Photos/3.5_stars.png",style [("width","100%")]] []
+                                ,img [src "3.5_stars.png",style [("width","100%")]] []
                                 ,li [] [text "Collaboration"]
-                                ,img [src "CV_Photos/4_stars.png", style [("width","100%")]] []
+                                ,img [src "4_stars.png", style [("width","100%")]] []
                                 ,li [] [text "Organization"]
-                                ,img [src "CV_Photos/4_stars.png", style [("width","100%")]] []
+                                ,img [src "4_stars.png", style [("width","100%")]] []
                                 ,li [] [text "Responibility"]
-                                ,img [src "CV_Photos/4_stars.png", style [("width","100%")]] []
+                                ,img [src "4_stars.png", style [("width","100%")]] []
                                 ]
                           ]
 
@@ -94,7 +94,7 @@ main = div [divStyle] [
                                             ,li [] [text "Certificate of Completion for IT Essentials (Cisco Networking Academy)"]
                                             ,li [] [text "Successfully completed 25 problems on https://projecteuler.net"]
                                             ,li [] [text "Used the Elm architecture to create ",a [href "Scared_Elephant.html"] [text "The Scared Elephant"]]
-                                            ,li [] [text "Link to back to other CV page " ,a [href "index.html"] [text "Here"]]
+                                            ,li [] [text "Link to alternate CV page created with Elm" ,a [href "index2.html"] [text "Here"]]
                                             ,li [] [text "Link to personal GitHub ",a [href "https://github.com/connorhewick"] [text "here"]]
                                             ]
                                     ]
